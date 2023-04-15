f763040b6c02b27883ffcd808b6de3e273f884dc
kharisovilyas <91402961+kharisovilyas@users.noreply.github.com>
2023-04-15 17:20:37 +0300

    for ROMA

--- src/main/java/com/example/synt5/Synt5Application.java
+++ src/main/java/com/example/synt5/Synt5Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Synt5Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Synt5Application.class, args);
     }
-
 }
