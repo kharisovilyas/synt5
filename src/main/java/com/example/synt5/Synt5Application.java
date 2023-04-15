06a5a987818b74c70a96db8f7fa14e8ff215c927
kharisovilyas <91402961+kharisovilyas@users.noreply.github.com>
2023-04-15 16:27:27 +0300

    Initial commit

---
+++ src/main/java/com/example/synt5/Synt5Application.java
@@ -0,0 +1,13 @@
+package com.example.synt5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Synt5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Synt5Application.class, args);
+    }
+
+}
